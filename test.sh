040777ca2c0fe5c3d545f3541be4bfd10efc4bcd
Stephen G. Pope <stephengpope@gmail.com>
2026-02-05 19:33:15 -0800

    deleted test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --rm \
-  -e GH_TOKEN="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" \
-  -e REPO_URL="https://github.com/stephengpope/thepopebot.git" \
-  -e BRANCH="job/test-3" \
-  thepopebot
